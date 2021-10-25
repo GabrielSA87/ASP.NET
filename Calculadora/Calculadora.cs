87c44ff773543c495dc36ca07606c21b35143306
Gabriel Araujo <72812005+GabrielSA87@users.noreply.github.com>
2021-10-25 15:52:58 -0300

    Add files via upload

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Calculadora : Form
+    {
+        double total;
+        double ultimoNumero;
+        string operador;
+
+        public void Limpar()
+        {
+            total = 0;
+            ultimoNumero = 0;
+            operador = "+";
+            txtDisplay.Text = "0";
+        }
+
+        private void Calcular()
+        {
+            switch (operador)
+            {
+                case "+": total = total + ultimoNumero;
+                    break;
+                case "-": total = total - ultimoNumero;
+                    break;
+                case "/": total = total / ultimoNumero;
+                    break;
+                case "*": total = total * ultimoNumero;
+                    break;
+            }
+
+            ultimoNumero = 0;
+            txtDisplay.Text = total.ToString();
+        }
+
+        public Calculadora()
+        {
+            InitializeComponent();
+            Limpar();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Limpar();
+        }
+        
+        private void gerarNumero(object sender, EventArgs e)
+        {
+            if (ultimoNumero == 0)
+            {
+                txtDisplay.Text = (sender as Button).Text;
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text + (sender as Button).Text;
+            }
+            
+            ultimoNumero = Convert.ToDouble(txtDisplay.Text);
+        }
+
+        private void operadores(object sender, EventArgs e)
+        {
+            ultimoNumero = Convert.ToDouble(txtDisplay.Text);
+            Calcular();
+            operador = (sender as Button).Text;
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            ultimoNumero = Convert.ToDouble(txtDisplay.Text);
+            Calcular();
+            operador = "+";
+            total = 0;
+        }
+    }
+}
